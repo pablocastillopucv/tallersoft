e3927a8f1ffb1b0679a2f89736d61ce37b65cacb
Andres Carcamo G <carcamo_8@hotmail.com>
2019-08-25 01:32:22 -0400

    Niveles pggooooo
    Modificacion de gameover en game.js

--- assets/Script/Circle.js
+++ assets/Script/Circle.js
@@ -11,24 +11,21 @@ cc.Class({
     onPicked: function() {
         this.node.destroy();
         this.game.spawnNewCircle();
-        // then destroy the current star's node
-  
-        
-      
+
     },
     update: function(dt){
         
         this.timer +=dt;
         if(this.game.aux< this.game.cantVeces){
 
-            if ( Math.round(this.timer) == 3.0 ) {
+            if ( Math.round(this.timer) == 1.0 ) {
                 this.game.aux++;
                 this.onPicked();
                 return;
             }
         }
         else{
-            if ( Math.round(this.timer) == 3.0 ) {
+            if ( Math.round(this.timer) == 1.0 ) {
                 
                 this.node.destroy();
                 this.game.aux=0;
